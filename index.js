2abf383814028aed3e268429b9a59bd76e261f33
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-12-04 13:59:44 -0300

    Aplicação de testes unitários básicos em index.js

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
-somaHorasExtras(2000, 500);
 
-const calculaDescontos = (salario, descontos) => salario
+const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const teste = (titulo, esperado, retornado) => {
-   if (esperado === retornado) {
-    console.log(`${titulo} passou`);
-   } else {
-    console.error(`${titulo} não passou`);
-   }
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        throw {};
+      }
+    },
+  };
+  return assercoes;
 };
 
-teste(`somaHorasExtras`, 2500, somaHorasExtras(2000));
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passou!`);
+  } catch {
+    console.error(`${titulo} não passou!`);
+  }
+};
 
-teste(`calculoDescontos`, 2500, calculaDescontos(2000));
+// const teste = (titulo, esperado, retornado) => {
+//    if (esperado === retornado) {
+//     console.log(`${titulo} passou`);
+//    } else {
+//     console.error(`${titulo} não passou`);
+//    }
+// };
+
+teste('somaHorasExtras', () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+// teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+
+teste('calculaDesconto', () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+// teste('calculoDescontos', 2500, calculaDescontos(2000, 300));
